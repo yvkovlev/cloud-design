3aef8e72e1cc9555086dfc550c666ae841b47f84
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-12-17 17:32:58 +0300

    add format schema and fix balance.js

--- src/routes/balance.js
+++ src/routes/balance.js
@@ -6,8 +6,8 @@ const User = require("../models/User");
 
 router
   .get("/balance", async (req, res) => {
-    const user = await User.find({ email: req.body.email});
-    const current = await Balance.find({ u_id: user._id}, (err, balance) => {
+    const user = await User.findOne({ email: req.body.email}).lean();
+    const current = await Balance.find({ u_id: user._id }, (err, balance) => {
       if (err)
         res.status(500).send({
           "code": "500",
@@ -16,48 +16,43 @@ router
       else
         res.status(200).send(balance);
     })
-
   })
 
   .post("/balance", async (req, res) => {
     const user = User.findOne({});
     res.json(user);
-    // const current = Balance.find({ u_id: userinho._id }, (err, balance) => {
-    //   if (err)
-    //     res.status(500)
-    //       .send({
-    //         "code": "500",
-    //         "message": "server error"
-    //       })
-    // });
-    // const newBalanceAdding = new BalanceAdding({
-    //   b_id: current._id,
-    //   value_hours: req.body.hours,
-    //   value_rubbles: req.body.cost
-    // })
-    // const balanceUpdate = Balance.findOneAndUpdate(
-    //   { u_id: user._id },
-    //   {
-    //     $set: {
-    //       hours: current.hours + req.body.hours,
-    //       rubbles: current.rubbles + req.body.cost
-    //     }
-    //   },
-    //   (err) => {
-    //     if (err) {
-    //       return res.status(500)
-    //         .send({
-    //           "code": "500",
-    //           "message": "server error"
-    //         })
-    //     } else {
-    //       return res.status(200)
-    //         .send({
-    //           "code": "200",
-    //           "message": ""
-    //         })
-    //     }
-    //   })
-  })
+    const currentBalance = await Balance.findOne({ u_id: user._id }).lean();
+
+    const newBalanceAdding = new BalanceAdding({
+      b_id: current._id,
+      value_hours: req.body.hours,
+      value_rubbles: req.body.cost
+    });
+    newBalanceAdding.save();
+
+    const balanceUpdate = Balance.findOneAndUpdate(
+      { u_id: user._id },
+      {
+        $set: {
+          hours: current.hours + req.body.hours,
+          rubbles: current.rubbles + req.body.cost
+        }
+      },
+      (err) => {
+        if (err) {
+          return res.status(500)
+            .send({
+              "code": "500",
+              "message": "server error"
+            })
+        } else {
+          return res.status(200)
+            .send({
+              "code": "200",
+              "message": ""
+            })
+        }
+      })
+  });
 
 module.exports = router;

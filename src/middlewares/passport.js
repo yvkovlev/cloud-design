4389fe4e35bfff96bcb41d0f171a128d04a8caca
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-12-29 20:21:32 +0300

    fix returning code

--- src/middlewares/passport.js
+++ src/middlewares/passport.js
@@ -1,45 +1,9 @@
-// const JwtStrategy = require('passport-jwt').Strategy;
-// const ExtractJwt = require('passport-jwt').ExtractJwt;
-// const mongoose = require('mongoose');
-// const User = require('../models/User');
-//
-// const options = {
-//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//   secretOrKey: 'jseohfn'
-// }
-//
-// module.exports = passport => {
-//   passport.use(
-//     new JwtStrategy(options, async (payload, done) => {
-//       console.log('imbuli');
-//       User.findOne({ email: "ale@gmail.com"}, (err, user) => {
-//         if (err) {
-//           console.log('nib')
-//         } else {
-//           console.log('not nib')
-//         }
-//       });
-//       User.findOne({_id: payload.userId}, (err, user) => {
-//         try {
-//           if (user) {
-//             done(null, user)
-//           } else {
-//             done(null, false)
-//           }
-//         } catch (e) {
-//           console.log(e);
-//         }
-//       });
-//     })
-//   )
-// }
-
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.header('token');
   if (!token) return res.send({
-    "status": "401",
+    "status": 403,
     "message": "access denied"
   });
 
@@ -49,7 +13,7 @@ module.exports = (req, res, next) => {
     next();
   } catch (err) {
     res.send({
-      "status": "400",
+      "status": 401,
       "message": "invalid token"
     })
   }

71b2cf81a5e95241acbb888c6d2c28354e1b2b27
yvkovlev <barkov777@gmail.com>
2021-01-02 13:59:14 +0300

    Footer added

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -7,7 +7,13 @@ const Footer = () => {
   } = styles;
   return (
     <footer className={footer}>
-
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <small className="text-muted">Cloud Design © 2021. Все права защищены.</small>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
